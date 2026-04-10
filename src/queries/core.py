217dc3e99b4248ab84cd3a388d66872c4c4bf0e8
Ornella Sultanbekova <nella911107@gmail.com>
2026-04-10 17:32:30 +0300

    without raw SQL queries

--- src/queries/core.py
+++ src/queries/core.py
@@ -1,25 +1,33 @@
-from sqlalchemy import text
+from sqlalchemy import text, insert, select
 
 from database import sync_engine
-from models import metadata_obj
+from models import metadata_obj, workers_table
+
 
 def create_tables():
-    metadata_obj.echo = False
+    # sync_engine.echo = False
     metadata_obj.drop_all(sync_engine)
     metadata_obj.create_all(sync_engine)
-    metadata_obj.echo = True
+    # sync_engine.echo = True
 
 
 def insert_data():
     with sync_engine.connect() as conn:
-        stmt = "INSERT INTO workers (username) VALUES ('John Doe'), ('Ivan Ivanov');"
-        conn.execute(text(stmt))
+        # stmt = "INSERT INTO workers (username) VALUES ('John Doe'), ('Ivan Ivanov');"
+        stmt = insert(workers_table).values(
+            [
+                {"username": "John Doe"},
+                {"username": "Ivan Ivanov"},
+            ]
+        )
+        conn.execute(stmt)
         conn.commit()
 
 
 def select_data():
     with sync_engine.connect() as conn:
-        stmt = "SELECT * FROM workers;"
-        result = conn.execute(text(stmt))
+        # stmt = "SELECT * FROM workers;"
+        stmt = select(workers_table)
+        result = conn.execute(stmt)
         for row in result:
             print(row)
