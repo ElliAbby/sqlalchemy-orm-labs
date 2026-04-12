1475a37eca932c083dce58c146e55207eacb2624
Ornella Sultanbekova <nella911107@gmail.com>
2026-04-12 11:32:36 +0300

    added class in core

--- src/main.py
+++ src/main.py
@@ -1,35 +1,52 @@
+import logging
 import os
 import sys
-import logging
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
 
 # императивный подход
-# from queries.core import create_tables, insert_data, select_data
+from queries.core import SyncCore
 
 # декларативный подход
-from queries.orm import create_tables, insert_data, select_data
+# from queries.orm import create_tables, insert_data, select_data
 
 logger = logging.getLogger(__name__)
 
 try:
     logger.info("Creating tables...")
-    create_tables()
+    SyncCore.create_tables()
     logger.info("Tables created successfully")
 except Exception as e:
-    logger.info(f"Error creating tables: {e}")
+    logger.warning(f"Error creating tables: {e}")
 
 
 try:
     logger.info("Inserting data...")
-    insert_data()
+    SyncCore.insert_workers()
     logger.info("Data inserted successfully")
 except Exception as e:
-    logger.info(f"Error inserting data: {e}")
+    logger.warning(f"Error inserting data: {e}")
 
 
 try:
     logger.info("Selecting data...")
-    select_data()
+    SyncCore.select_workers()
     logger.info("Data selected successfully")
 except Exception as e:
-    logger.info(f"Error selecting data: {e}")
+    logger.warning(f"Error selecting data: {e}")
+
+
+try:
+    logger.info("Update worker where id=1")
+    SyncCore.update_worker(worker_id=1, new_username="Superman")
+    logger.info("Data updated successfuly")
+except Exception as e:
+    logger.warning(f"Error updating data: {e}")
+
+
+try:
+    logger.info("Delete worker where id=1")
+    SyncCore.delete_worker(worker_id=1)
+    logger.info("Data deleted successfuly")
+except Exception as e:
+    logger.warning(f"Error deleting data: {e}")
--- src/queries/core.py
+++ src/queries/core.py
@@ -1,33 +1,57 @@
-from sqlalchemy import text, insert, select
+from sqlalchemy import delete, insert, select, text, update
 
 from database import sync_engine
 from models import metadata_obj, workers_table
 
 
-def create_tables():
-    # sync_engine.echo = False
-    metadata_obj.drop_all(sync_engine)
-    metadata_obj.create_all(sync_engine)
-    # sync_engine.echo = True
+class SyncCore:
+    @staticmethod
+    def create_tables():
+        # sync_engine.echo = False
+        metadata_obj.drop_all(sync_engine)
+        metadata_obj.create_all(sync_engine)
+        # sync_engine.echo = True
 
+    @staticmethod
+    def insert_workers():
+        with sync_engine.connect() as conn:
+            # stmt = "INSERT INTO workers (username) VALUES ('John Doe'), ('Ivan Ivanov');"
+            stmt = insert(workers_table).values(
+                [
+                    {"username": "John Doe"},
+                    {"username": "Ivan Ivanov"},
+                ]
+            )
+            conn.execute(stmt)
+            conn.commit()
 
-def insert_data():
-    with sync_engine.connect() as conn:
-        # stmt = "INSERT INTO workers (username) VALUES ('John Doe'), ('Ivan Ivanov');"
-        stmt = insert(workers_table).values(
-            [
-                {"username": "John Doe"},
-                {"username": "Ivan Ivanov"},
-            ]
-        )
-        conn.execute(stmt)
-        conn.commit()
+    @staticmethod
+    def select_workers():
+        with sync_engine.connect() as conn:
+            # stmt = "SELECT * FROM workers;"
+            stmt = select(workers_table)
+            result = conn.execute(stmt)
+            for row in result:
+                print(row)
 
+    @staticmethod
+    def update_worker(worker_id: int, new_username: str):
+        with sync_engine.connect() as conn:
+            # stmt = text("UPDATE workers SET username=:username WHERE id=:id")
+            # stmt = stmt.bindparams(username=new_username, id=worker_id)
+            stmt = (
+                update(workers_table)
+                .values(username=new_username)
+                .filter_by(id=worker_id)
+            )
+            conn.execute(stmt)
+            conn.commit()
 
-def select_data():
-    with sync_engine.connect() as conn:
-        # stmt = "SELECT * FROM workers;"
-        stmt = select(workers_table)
-        result = conn.execute(stmt)
-        for row in result:
-            print(row)
+    @staticmethod
+    def delete_worker(worker_id: int):
+        with sync_engine.connect() as conn:
+            # stmt = text("DELETE FROM workers WHERE id=:id")
+            # stmt = stmt.bindparams(id=worker_id)
+            stmt = delete(workers_table).filter_by(id=worker_id)
+            conn.execute(stmt)
+            conn.commit()
